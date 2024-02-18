d208c19beda8d61cfabf34e16b4d8dcdd61dd438
ahmedMG21 <2040057@uj.edu.sa>
2024-02-18 11:26:15 +0300

    google btn deleted

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 }
